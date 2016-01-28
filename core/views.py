11757a60f0a12913092f6db18e07c0059ae48b75
naveen <naveen@nhst.no>
2016-01-28 19:05:51 +0100

    initial commit for django

---
+++ core/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.conf import settings
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from mlapi.DatumBox import DatumBox
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'core/index.html')
+
+
+@api_view(['GET', 'POST'])
+def classify(request):
+    """
+    List all snippets, or create a new snippet.
+    """
+    if request.method == 'POST':
+        text = request.POST.get('text')
+        data = {}
+        classifier = DatumBox(settings.API_KEY)
+        data.update({"result": classifier.sentiment_analysis(text)})
+        return Response(data)
+
+    else:
+        data = {}
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
