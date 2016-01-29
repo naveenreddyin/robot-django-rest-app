374b7914f81819b22fc7c25b39513df6730a2e46
naveen <naveen@nhst.no>
2016-01-29 02:22:41 +0100

    unicode error handled

--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
+import ast
+
 from itertools import izip
 
 from django.http import HttpResponse
@@ -48,14 +50,23 @@ def entities(request):
         module_id = settings.MONKEYLEARN_MODULE_ID
         res = ml.extractors.extract(module_id, text_list)
         print(res.result)
-        # for i in range(len(res.result[0])):
-        #     data.append(res.result[0][i])
-        data.append({'count': 1, 'tag': 'ORGANIZATION', 'entity': 'About Data Respons Data Respons'})
-        data.append({'count': 1, 'tag': 'ORGANIZATION', 'entity': 'Oslo Stock Exchange'})
-        data.append({'count': 3, 'tag': 'PERSON', 'entity': 'Ragnvaldsen'})
+        for i in range(len(res.result[0])):
+            data.append(convert(res.result[0][i]))
+        # data.append({'count': 1, 'tag': 'ORGANIZATION', 'entity': 'About Data Respons Data Respons'})
+        # data.append({'count': 1, 'tag': 'ORGANIZATION', 'entity': 'Oslo Stock Exchange'})
+        # data.append({'count': 3, 'tag': 'PERSON', 'entity': 'Ragnvaldsen'})
         return Response(data)
 
     else:
         data = {}
         return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
+def convert(input):
+    if isinstance(input, dict):
+        return {convert(key): convert(value) for key, value in input.iteritems()}
+    elif isinstance(input, list):
+        return [convert(element) for element in input]
+    elif isinstance(input, unicode):
+        return input.encode('utf-8')
+    else:
+        return input
